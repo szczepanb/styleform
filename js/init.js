bb846693a73d6a4c02142c6bcaf30136a6c4e4ba
Szczepan Bielecki <bielecki.szczepan@gmail.com>
2018-07-22 11:06:08 +0200

    Checkbox and Radio. Optimize events, and create script for performance. Add Keyboard events. Change html structure - remove a tag, replaced by span better work. Add tab index and default style active element.

--- js/init.js
+++ js/init.js
@@ -1,5 +1,24 @@
 $(function()
 {
+	$(".sf-form").validate(
+	{
+		ignore: '.ignore-validation',
+		errorPlacement: function(error, element)
+		{
+			if($(element).prop("checkbox") === true && $(element).closest('.sf-ch-wrapper'))
+			{
+				error.insertAfter($(element).closest('.sf-ch-wrapper'));
+		    }
+			else if($(element).prop("radio")  === true && $(element).closest('.sf-r-wrapper'))
+			{
+				 error.insertAfter($("input[name='"+element.attr('name')+"']").last().closest('.sf-r-wrapper'));
+		    }
+		    else {
+		        error.insertAfter(element);
+		    }
+		}
+	})
+	
 	$(".styleform input[type='checkbox']").superCheckbox();
 	$(".styleform input[type='radio']").superRadio();
 	
